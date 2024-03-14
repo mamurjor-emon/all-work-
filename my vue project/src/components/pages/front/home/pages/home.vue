bd8026f2312dedfbbb03d24f92cdfae0d1d8a898
mamurjor-mahmud <mamurjor.mahmud@gmail.com>
2024-03-14 15:10:42 +0600

    added code of laravel dashborad

--- my vue project/src/components/pages/front/home/pages/home.vue
+++ my vue project/src/components/pages/front/home/pages/home.vue
@@ -1,6 +1,74 @@
 <template>
-    <div>
-        <section class="section-hero">
+  <div>
+    <section class="section-hero">
+      <!-- 1.  Now we need to make our hero section display grid and need to center the content in the page.
+    
+           2. To acheive that we need to create a div container inside of our section that contains all the text and img -->
+
+      <div class="hero">
+        <div
+          class="hero-text-box"
+          data-aos="fade-left"
+          data-aos-offset="500"
+          data-aos-easing="ease-in-sine"
+        >
+          <h1 class="heading-primary">
+            A healthy meal delivered to your door, every single day
+          </h1>
+          <p class="hero-description">
+            The smart 365-days-per-year food subscription that will make you eat healthy
+            again. Tailored to your personal tastes and nutritional needs.
+          </p>
+          <a href="#" class="btn btn--fill margin-right-btn">Start eating well </a>
+          <a href="#" class="btn btn--outline margin-right-btn">Learn more &darr; </a>
+        </div>
+        <div
+          class="hero-img-box"
+          data-aos="fade-right"
+          data-aos-anchor="#example-anchor"
+          data-aos-offset="500"
+          data-aos-duration="500"
+        >
+          <img
+            src="https://prayagtandon.github.io/Omnifood-Project/Hero-section/img/hero.png"
+            alt="Woman enjoying food, meals in storage container, and food bowls on a table"
+            class="hero-img"
+          />
+        </div>
+        <div class="delivered-meals">
+          <div class="delivered-imgs">
+            <img
+              src="https://prayagtandon.github.io/Omnifood-Project/Hero-section/img/customers/customer-1.jpg"
+              alt="Customer photo"
+            />
+            <img
+              src="https://prayagtandon.github.io/Omnifood-Project/Hero-section/img/customers/customer-2.jpg"
+              alt="Customer photo"
+            />
+            <img
+              src="https://prayagtandon.github.io/Omnifood-Project/Hero-section/img/customers/customer-3.jpg"
+              alt="Customer photo"
+            />
+            <img
+              src="https://prayagtandon.github.io/Omnifood-Project/Hero-section/img/customers/customer-4.jpg"
+              alt="Customer photo"
+            />
+            <img
+              src="https://prayagtandon.github.io/Omnifood-Project/Hero-section/img/customers/customer-5.jpg"
+              alt="Customer photo"
+            />
+            <img
+              src="https://prayagtandon.github.io/Omnifood-Project/Hero-section/img/customers/customer-6.jpg"
+              alt="Customer photo"
+            />
+          </div>
+          <p class="delivered-text"><span>250,000+</span> meals delivered last year!</p>
+        </div>
+      </div>
+    </section>
+  </div>
+  <div>
+    <section data-aos="fade-up" class="section-hero">
       <!-- 1.  Now we need to make our hero section display grid and need to center the content in the page.
     
            2. To acheive that we need to create a div container inside of our section that contains all the text and img -->
@@ -11,16 +79,11 @@
             A healthy meal delivered to your door, every single day
           </h1>
           <p class="hero-description">
-            The smart 365-days-per-year food subscription that will make you eat
-            healthy again. Tailored to your personal tastes and nutritional
-            needs.
+            The smart 365-days-per-year food subscription that will make you eat healthy
+            again. Tailored to your personal tastes and nutritional needs.
           </p>
-          <a href="#" class="btn btn--fill margin-right-btn"
-            >Start eating well
-          </a>
-          <a href="#" class="btn btn--outline margin-right-btn"
-            >Learn more &darr;
-          </a>
+          <a href="#" class="btn btn--fill margin-right-btn">Start eating well </a>
+          <a href="#" class="btn btn--outline margin-right-btn">Learn more &darr; </a>
         </div>
         <div class="hero-img-box">
           <img
@@ -31,18 +94,273 @@
         </div>
         <div class="delivered-meals">
           <div class="delivered-imgs">
-            <img src="https://prayagtandon.github.io/Omnifood-Project/Hero-section/img/customers/customer-1.jpg" alt="Customer photo" />
-            <img src="https://prayagtandon.github.io/Omnifood-Project/Hero-section/img/customers/customer-2.jpg" alt="Customer photo" />
-            <img src="https://prayagtandon.github.io/Omnifood-Project/Hero-section/img/customers/customer-3.jpg" alt="Customer photo" />
-            <img src="https://prayagtandon.github.io/Omnifood-Project/Hero-section/img/customers/customer-4.jpg" alt="Customer photo" />
-            <img src="https://prayagtandon.github.io/Omnifood-Project/Hero-section/img/customers/customer-5.jpg" alt="Customer photo" />
-            <img src="https://prayagtandon.github.io/Omnifood-Project/Hero-section/img/customers/customer-6.jpg" alt="Customer photo" />
+            <img
+              src="https://prayagtandon.github.io/Omnifood-Project/Hero-section/img/customers/customer-1.jpg"
+              alt="Customer photo"
+            />
+            <img
+              src="https://prayagtandon.github.io/Omnifood-Project/Hero-section/img/customers/customer-2.jpg"
+              alt="Customer photo"
+            />
+            <img
+              src="https://prayagtandon.github.io/Omnifood-Project/Hero-section/img/customers/customer-3.jpg"
+              alt="Customer photo"
+            />
+            <img
+              src="https://prayagtandon.github.io/Omnifood-Project/Hero-section/img/customers/customer-4.jpg"
+              alt="Customer photo"
+            />
+            <img
+              src="https://prayagtandon.github.io/Omnifood-Project/Hero-section/img/customers/customer-5.jpg"
+              alt="Customer photo"
+            />
+            <img
+              src="https://prayagtandon.github.io/Omnifood-Project/Hero-section/img/customers/customer-6.jpg"
+              alt="Customer photo"
+            />
           </div>
-          <p class="delivered-text">
-            <span>250,000+</span> meals delivered last year!
-          </p>
+          <p class="delivered-text"><span>250,000+</span> meals delivered last year!</p>
         </div>
       </div>
     </section>
-    </div>
+  </div>
+  <div>
+    <section data-aos="fade-up" class="section-hero">
+      <!-- 1.  Now we need to make our hero section display grid and need to center the content in the page.
+    
+           2. To acheive that we need to create a div container inside of our section that contains all the text and img -->
+
+      <div class="hero">
+        <div class="hero-text-box">
+          <h1 class="heading-primary">
+            A healthy meal delivered to your door, every single day
+          </h1>
+          <p class="hero-description">
+            The smart 365-days-per-year food subscription that will make you eat healthy
+            again. Tailored to your personal tastes and nutritional needs.
+          </p>
+          <a href="#" class="btn btn--fill margin-right-btn">Start eating well </a>
+          <a href="#" class="btn btn--outline margin-right-btn">Learn more &darr; </a>
+        </div>
+        <div class="hero-img-box">
+          <img
+            src="https://prayagtandon.github.io/Omnifood-Project/Hero-section/img/hero.png"
+            alt="Woman enjoying food, meals in storage container, and food bowls on a table"
+            class="hero-img"
+          />
+        </div>
+        <div class="delivered-meals">
+          <div class="delivered-imgs">
+            <img
+              src="https://prayagtandon.github.io/Omnifood-Project/Hero-section/img/customers/customer-1.jpg"
+              alt="Customer photo"
+            />
+            <img
+              src="https://prayagtandon.github.io/Omnifood-Project/Hero-section/img/customers/customer-2.jpg"
+              alt="Customer photo"
+            />
+            <img
+              src="https://prayagtandon.github.io/Omnifood-Project/Hero-section/img/customers/customer-3.jpg"
+              alt="Customer photo"
+            />
+            <img
+              src="https://prayagtandon.github.io/Omnifood-Project/Hero-section/img/customers/customer-4.jpg"
+              alt="Customer photo"
+            />
+            <img
+              src="https://prayagtandon.github.io/Omnifood-Project/Hero-section/img/customers/customer-5.jpg"
+              alt="Customer photo"
+            />
+            <img
+              src="https://prayagtandon.github.io/Omnifood-Project/Hero-section/img/customers/customer-6.jpg"
+              alt="Customer photo"
+            />
+          </div>
+          <p class="delivered-text"><span>250,000+</span> meals delivered last year!</p>
+        </div>
+      </div>
+    </section>
+  </div>
+  <div>
+    <section data-aos="fade-up" class="section-hero">
+      <!-- 1.  Now we need to make our hero section display grid and need to center the content in the page.
+    
+           2. To acheive that we need to create a div container inside of our section that contains all the text and img -->
+
+      <div class="hero">
+        <div class="hero-text-box">
+          <h1 class="heading-primary">
+            A healthy meal delivered to your door, every single day
+          </h1>
+          <p class="hero-description">
+            The smart 365-days-per-year food subscription that will make you eat healthy
+            again. Tailored to your personal tastes and nutritional needs.
+          </p>
+          <a href="#" class="btn btn--fill margin-right-btn">Start eating well </a>
+          <a href="#" class="btn btn--outline margin-right-btn">Learn more &darr; </a>
+        </div>
+        <div class="hero-img-box">
+          <img
+            src="https://prayagtandon.github.io/Omnifood-Project/Hero-section/img/hero.png"
+            alt="Woman enjoying food, meals in storage container, and food bowls on a table"
+            class="hero-img"
+          />
+        </div>
+        <div class="delivered-meals">
+          <div class="delivered-imgs">
+            <img
+              src="https://prayagtandon.github.io/Omnifood-Project/Hero-section/img/customers/customer-1.jpg"
+              alt="Customer photo"
+            />
+            <img
+              src="https://prayagtandon.github.io/Omnifood-Project/Hero-section/img/customers/customer-2.jpg"
+              alt="Customer photo"
+            />
+            <img
+              src="https://prayagtandon.github.io/Omnifood-Project/Hero-section/img/customers/customer-3.jpg"
+              alt="Customer photo"
+            />
+            <img
+              src="https://prayagtandon.github.io/Omnifood-Project/Hero-section/img/customers/customer-4.jpg"
+              alt="Customer photo"
+            />
+            <img
+              src="https://prayagtandon.github.io/Omnifood-Project/Hero-section/img/customers/customer-5.jpg"
+              alt="Customer photo"
+            />
+            <img
+              src="https://prayagtandon.github.io/Omnifood-Project/Hero-section/img/customers/customer-6.jpg"
+              alt="Customer photo"
+            />
+          </div>
+          <p class="delivered-text"><span>250,000+</span> meals delivered last year!</p>
+        </div>
+      </div>
+    </section>
+  </div>
+  <div>
+    <section data-aos="fade-up" class="section-hero">
+      <!-- 1.  Now we need to make our hero section display grid and need to center the content in the page.
+    
+           2. To acheive that we need to create a div container inside of our section that contains all the text and img -->
+
+      <div class="hero">
+        <div class="hero-text-box">
+          <h1 class="heading-primary">
+            A healthy meal delivered to your door, every single day
+          </h1>
+          <p class="hero-description">
+            The smart 365-days-per-year food subscription that will make you eat healthy
+            again. Tailored to your personal tastes and nutritional needs.
+          </p>
+          <a href="#" class="btn btn--fill margin-right-btn">Start eating well </a>
+          <a href="#" class="btn btn--outline margin-right-btn">Learn more &darr; </a>
+        </div>
+        <div class="hero-img-box">
+          <img
+            src="https://prayagtandon.github.io/Omnifood-Project/Hero-section/img/hero.png"
+            alt="Woman enjoying food, meals in storage container, and food bowls on a table"
+            class="hero-img"
+          />
+        </div>
+        <div class="delivered-meals">
+          <div class="delivered-imgs">
+            <img
+              src="https://prayagtandon.github.io/Omnifood-Project/Hero-section/img/customers/customer-1.jpg"
+              alt="Customer photo"
+            />
+            <img
+              src="https://prayagtandon.github.io/Omnifood-Project/Hero-section/img/customers/customer-2.jpg"
+              alt="Customer photo"
+            />
+            <img
+              src="https://prayagtandon.github.io/Omnifood-Project/Hero-section/img/customers/customer-3.jpg"
+              alt="Customer photo"
+            />
+            <img
+              src="https://prayagtandon.github.io/Omnifood-Project/Hero-section/img/customers/customer-4.jpg"
+              alt="Customer photo"
+            />
+            <img
+              src="https://prayagtandon.github.io/Omnifood-Project/Hero-section/img/customers/customer-5.jpg"
+              alt="Customer photo"
+            />
+            <img
+              src="https://prayagtandon.github.io/Omnifood-Project/Hero-section/img/customers/customer-6.jpg"
+              alt="Customer photo"
+            />
+          </div>
+          <p class="delivered-text"><span>250,000+</span> meals delivered last year!</p>
+        </div>
+      </div>
+    </section>
+  </div>
+  <div>
+    <section data-aos="fade-up" class="section-hero">
+      <!-- 1.  Now we need to make our hero section display grid and need to center the content in the page.
+    
+           2. To acheive that we need to create a div container inside of our section that contains all the text and img -->
+
+      <div class="hero">
+        <div class="hero-text-box">
+          <h1 class="heading-primary">
+            A healthy meal delivered to your door, every single day
+          </h1>
+          <p class="hero-description">
+            The smart 365-days-per-year food subscription that will make you eat healthy
+            again. Tailored to your personal tastes and nutritional needs.
+          </p>
+          <a href="#" class="btn btn--fill margin-right-btn">Start eating well </a>
+          <a href="#" class="btn btn--outline margin-right-btn">Learn more &darr; </a>
+        </div>
+        <div class="hero-img-box">
+          <img
+            src="https://prayagtandon.github.io/Omnifood-Project/Hero-section/img/hero.png"
+            alt="Woman enjoying food, meals in storage container, and food bowls on a table"
+            class="hero-img"
+          />
+        </div>
+        <div class="delivered-meals">
+          <div class="delivered-imgs">
+            <img
+              src="https://prayagtandon.github.io/Omnifood-Project/Hero-section/img/customers/customer-1.jpg"
+              alt="Customer photo"
+            />
+            <img
+              src="https://prayagtandon.github.io/Omnifood-Project/Hero-section/img/customers/customer-2.jpg"
+              alt="Customer photo"
+            />
+            <img
+              src="https://prayagtandon.github.io/Omnifood-Project/Hero-section/img/customers/customer-3.jpg"
+              alt="Customer photo"
+            />
+            <img
+              src="https://prayagtandon.github.io/Omnifood-Project/Hero-section/img/customers/customer-4.jpg"
+              alt="Customer photo"
+            />
+            <img
+              src="https://prayagtandon.github.io/Omnifood-Project/Hero-section/img/customers/customer-5.jpg"
+              alt="Customer photo"
+            />
+            <img
+              src="https://prayagtandon.github.io/Omnifood-Project/Hero-section/img/customers/customer-6.jpg"
+              alt="Customer photo"
+            />
+          </div>
+          <p class="delivered-text"><span>250,000+</span> meals delivered last year!</p>
+        </div>
+      </div>
+    </section>
+  </div>
 </template>
+
+<script>
+import AOS from "aos";
+import "aos/dist/aos.css";
+
+export default {
+  mounted() {
+    AOS.init();
+  },
+};
+</script>
